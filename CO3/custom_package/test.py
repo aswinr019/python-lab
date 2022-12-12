d6c64a6e5faf94edd4c16d681ba7f042b1432908
aswin r <119281218+aswinr019@users.noreply.github.com>
2022-12-11 20:49:43 -0800

    Update test.py

---
+++ CO3/custom_package/test.py
@@ -0,0 +1,16 @@
+
+from Graphics import Rectangle,Circle
+from Graphics.graphics_3D import Cuboid,Sphere
+
+print(Rectangle.area_rect(3,5))
+print(Rectangle.peri_rect(3,5))
+print(Circle.area_circ(3))
+print(Circle.area_circ(3))
+print(Cuboid.peri_cuboid(1,2,3))
+print(Cuboid.area_cuboid(1,2,3))
+print(Sphere.area_sphere(3))
+<<<<<<< HEAD
+print(Sphere.circumference_sphere(3))
+=======
+print(Sphere.circumference_sphere(3))
+>>>>>>> a7fa1510b1e9a75d3cf9110dc4615a34d9d2c24e
