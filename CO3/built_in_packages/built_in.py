515304c3c345f605772afd722048e76ba6bbdd0c
aswin r <119281218+aswinr019@users.noreply.github.com>
2022-12-22 01:25:38 +0000

    Update built_in.py

--- CO3/built_in_packages/built_in.py
+++ CO3/built_in_packages/built_in.py
@@ -1,25 +1,29 @@
 import os
 
+#get current path
 path = os.getcwd()
 
+#make directory if not exists
 if (os.path.exists(path+'NewFolder') == False):
     os.mkdir(path+"/NewFolder")
     print("Folder "+path+"NewFolder created.")
 else:
-    print(path+"NewFolder exists.")
-
+    print(path+"NewFolder exists."
+            )
+#make file if not exists
 if (os.path.exists(path+'/NewFolder/newfile.txt')):
     print("NewFile.txt already exists") 
     f = open(path+"/NewFolder/newfile.txt", "w")
 else:
     f = open(path+"/NewFolder/newfile.txt", "x")
 
-f.write("hai")
+f.write("hello world")
 f.close()
 
 #read from file
-# if (os.path.exists('C:/NewFolder/newfile.txt')):
-#     print("NewFile.txt already exists") 
-#     f = open("C:/NewFolder/newfile.txt", "w")
-# else:
-#     f = open("C:/NewFolder/newfile.txt", "x")
+if(os.path.exists(path+'/NewFolder/newfile.txt')):
+    f = open(path+'/NewFolder/newfile.txt','r')
+    print(f.read())
+    f.close()
+else :
+    print("No such file or direcory!")
