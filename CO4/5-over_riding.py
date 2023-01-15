9256f2acb71317316dcb94f6a4fec0e4e2f25a22
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-01-15 14:23:16 +0530

    Add files via upload

--- CO4/5-over_riding.py
+++ CO4/5-over_riding.py
@@ -1,10 +1,12 @@
 class Publisher : 
 
+    def __init__(self,name):
+        self.name = name
 
+    def display(self):
+        print(self.name)
 
-    
-
-class Book : 
+class Book(Publisher) : 
 
     def __int__(self,author,title):
         self.author = author 
@@ -12,9 +14,23 @@ class Book :
 
 
 
-class Python :
+class Python(Book) :
 
-    def __init(self,price,no_of_pages):
+    def __init__(self,price,no_of_pages):
         self.price = price
         self.no_of_pages = no_of_pages
+
+    def display(self):
+        print("Price : ",self.price)
+        print("No of pages : ",self.no_of_pages)
+
+
+pages = int(input("Enter the number of pages: "))
+price = int(input("Enter the price of the book: "))
+
+p = Python(price,pages)
+
+Python.display(p)
+
+
 
