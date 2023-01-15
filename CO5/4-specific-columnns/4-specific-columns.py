e2858259b07be998fd2dcf6d4b711b4efb5a09c7
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-01-15 13:13:38 +0530

    Add files via upload

--- CO5/4-specific-columnns/4-specific-columns.py
+++ CO5/4-specific-columnns/4-specific-columns.py
@@ -1,21 +1,16 @@
 import os
-import pandas
+import csv
 
 path = os.getcwd()
-cols = []
+
 
 if os.path.exists(path+"/username.csv"):
-    file = pandas.read_csv("username.csv")
-    columns = file.columns
+    with open(path+"/username.csv",newline='') as file:
+        reader = csv.DictReader(file)
+        
+        col = input("Enter the col to display(Username,Identifier,First name ,Last name) : ")
+        for row in reader:
+            print(row[col])
 
-    for col in columns:
-        cols.append(col)
-    print(cols)
-    
-    select = input("Enter the columns you want to display : ")
-
-    specific_cols = pandas.read_csv("username.csv",usecols = ["Username"] )
-
-    print(specific_cols)
 else:
     print("File doesn't exist!")
