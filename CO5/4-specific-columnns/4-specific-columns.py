cdd2fd59690f9ec37e7bb9e3211bb096f93e1958
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-01-15 13:21:26 +0530

    Add files via upload

--- CO5/4-specific-columnns/4-specific-columns.py
+++ CO5/4-specific-columnns/4-specific-columns.py
@@ -7,7 +7,6 @@ path = os.getcwd()
 if os.path.exists(path+"/username.csv"):
     with open(path+"/username.csv",newline='') as file:
         reader = csv.DictReader(file)
-        
         col = input("Enter the col to display(Username,Identifier,First name ,Last name) : ")
         for row in reader:
             print(row[col])
