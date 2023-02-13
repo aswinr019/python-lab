600ad675e78f9f467695f00e2c944a9a53261683
aswinr <aswinr019@gmail.com>
2023-02-13 18:36:37 +0530

    Minor changes

--- CO1/13-colors.py
+++ CO1/13-colors.py
@@ -1,5 +1,5 @@
 #create a list of colors from comma separated color names entered by user. Display first and last colors
 
-c = input("Enter colors ( coma separated ) ")
+c = input("Enter colors ( coma separated ): ")
 colors = c.split(',')
-print("The first and last colors are ",colors[0],colors[-1])
+print("The first and last colors are ",colors[0]," , ",colors[-1])
--- CO1/2-leep_year.py
+++ CO1/2-leep_year.py
@@ -5,6 +5,6 @@ year1  = int(input("Enter the base year : "))
 year2  = int(input("Enter the last year : "))
 
 print("Leep years between ",year1," and ",year2, " are  ")
-for i in range(year1,year2+1):
+for i in range(year1,year2):
     if(i % 4 == 0 or i % 400 == 0):
         print(i)
